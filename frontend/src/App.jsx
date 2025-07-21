ae0eb935455e779ecbcf0786dd56cc4ea0d2aca6
unknown <fghalandarii@gmail.com>
2025-07-21 16:22:55 -0400

    feat: Connect frontend to Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,13 +23,16 @@ function App() {
     // }, 300); // optional small delay
 
     try {
-      const response = await fetch("http://localhost:8000/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ url }),
-      });
+      const response = await fetch(
+        "https://skillmate-backend.onrender.com/generate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ url }),
+        }
+      );
 
       const data = await response.json();
       console.log("Backend response:", data);
